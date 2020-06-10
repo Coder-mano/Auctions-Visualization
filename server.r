d1864d43bf2791c8044ef9da1a1e78e74671f2d3
Bc. Jakub Ivan Vanko <jakub.ivan.vanko@student.tuke.sk>
2020-06-10 17:45:48 +0200

    Update server.r

--- server.r
+++ server.r
@@ -25,6 +25,21 @@ server <- function(input, output, session) {
     output$auctions <- renderAuctionPlot(input,output,session)
   })
   
+  #Bid progress 
+  output$bids_plot <- renderPlot({
+    subData2 = filter(offersInTime, Client == 100)
+    subData2 = filter(subData2, Auction_ID == input$A_ID)
+    subData2$New_BID = as.integer(subData2$New_BID)
+    ggplot(data = subData2, aes(x = subData2$Change_order, y = subData2$New_BID, colour = "red")) + geom_line() + theme_bw() + geom_point() + xlab("Number of bid") + ylab("Bid ammount") + theme(legend.position = "none")
+  })
+  
+  output$bids_table <- renderDataTable({
+    subData2 = filter(offersInTime, Client == 100)
+    subData2 = filter(subData2, Auction_ID == input$A_ID)
+    subData2$New_BID = as.integer(subData2$New_BID)
+    datatable(subData2, rownames = F,options = list(scrollX = TRUE))
+  })
+  
   # Authors
   output$authors <- renderTable({
     read_excel(paste('tasks', ".xlsx", sep=""), 1)
