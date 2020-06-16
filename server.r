611e1f9bc2b90ed5514d88db95e8669045772e33
Bc. Radoslav Bardovič <radoslav.bardovic@student.tuke.sk>
2020-06-16 14:27:12 +0200

    Update server.r

--- server.r
+++ server.r
@@ -23,7 +23,7 @@ server <- function(input, output, session) {
     evaluation_type = droplevels(evaluation_type, exclude = c("", "2722195"))
     evaluation_type = na.omit(evaluation_type)
     pie(table(evaluation_type), col = rainbow(7), main = "Auction Evaluation by",labels = "")
-    legend("bottomleft", legend = c("Pomocní výpočet - max", "Pomocní výpočet - min", "Celková nabítka účastníka", "Hodnocení", "Jednotlivé položky", "Multikriteriální hodnocení", "Skupiny položek" ),bty="n", fill = rainbow(7))
+    legend("bottomleft", legend = c("PomocnĂ­ vĂ˝poÄŤet - max", "PomocnĂ­ vĂ˝poÄŤet - min", "CelkovĂˇ nabĂ­tka ĂşÄŤastnĂ­ka", "HodnocenĂ­", "JednotlivĂ© poloĹľky", "MultikriteriĂˇlnĂ­ hodnocenĂ­", "Skupiny poloĹľek" ),bty="n", fill = rainbow(7))
     
   })
   
@@ -69,12 +69,32 @@ server <- function(input, output, session) {
     subData2$New_BID = as.integer(subData2$New_BID)
     datatable(subData2, rownames = F,options = list(scrollX = TRUE))
   })
-  # Items
+  # Items ---------------
+  library(tidyverse)
   output$items_plot = renderPlot({
     data3 = filter(items, Klient == input$klient)
+    data3=filter(data3,Auction_ID1 == input$aukcia)
+    #data3=filter(data3,Past_Price == input$aukcia )
+    data3$Past_Price = as.integer(data3$Past_Price)
+    ggplot(data3, aes(x=data3$Item_ID1, y=data3$Past_Price)) + xlab("Item ID")+ ylab("Past price") +
+      geom_segment( aes(x=data3$Item_ID1, xend=data3$Item_ID1, y=0, yend=data3$Past_Price)) +
+      geom_point( size=4, color="red", fill=alpha("orange", 0.3), alpha=0.7, shape=21, stroke=2)
+  
+    
+  })
+  output$items_plot1 = renderPlot({
+    data3 = filter(items, Klient == input$klient)
+    data3=filter(data3,Auction_ID1 == input$aukcia)
+    #data3=filter(data3,Past_Price == input$aukcia )
+    data3$Past_Price = as.integer(data3$Past_Price)
+    ggplot(data3, aes(x=data3$Item_ID1, y=data3$Quantity)) + xlab("Item ID")+ ylab("Quantity") +  
+      #geom_segment( aes(x=data3$Item_ID1, xend=data3$Item_ID1, y=0, yend=data3$Past_Price)) +
+      geom_point( size=4, color="red", fill=alpha("orange", 0.3), alpha=0.7, shape=21, stroke=2)
+    
   })
   output$items_table <- renderDataTable({
     data3 = filter(items, Klient == input$klient)
+    data3=filter(data3, Auction_ID1 == input$aukcia)
     datatable(data3)
   })
   
