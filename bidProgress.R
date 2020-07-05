62c602d383b90f75e1552fa10336ff0aa06aea39
Tomáš Adam <tomas.adam@student.tuke.sk>
2020-07-05 17:26:40 +0200

    Bid progress box update

--- bidProgress.R
+++ bidProgress.R
@@ -2,48 +2,44 @@
 # Bid progress UI.R
 
 bidProgress <- tabItem(tabName = "bids",
-                    fluidRow(
-                      box(
-                        width = 3,
-                        selectizeInput('bidType', 'Type', 
-                                       choices = na.omit(unique(allData$Type))
-                        )),
-                      box(
-                        width = 3,
-                        selectizeInput('bidClarification', 'Type Clarification', 
-                                       choices = c("") #observed na.omit(unique(allData$Type_Clarification))
-                        )),
-                      box(
-                        width = 3,
-                        selectizeInput('bidEvaluation', 'Evaluation', 
-                                       choices = c("") #observed #na.omit(unique(allData$EvaluatedBy))
-                        )),
-                      box(
-                        width = 3,
-                        selectizeInput('bidAuction', 'Auction ID', 
-                                       choices = c("") #observed #na.omit(unique(offersInTime$Auction_ID))
-                        )),
-                      box(
-                        width = 3,
-                        selectizeInput('bidItem', 'Item ID', 
-                                       choices = c("") #observed
-                        )),
-                      box(
-                        radioButtons("group", "Docasny button:",
-                                     c("Agregated" = "agregated",
-                                       "Participants" = "participants"))
-                      )
-                      
-                    ),
-                    
-                    fluidRow(
-                      box(
-                        width = 12,
-                        plotOutput("bidAuctions"),
-                        DT::dataTableOutput('bidDesc')
-                      )
-                    )
+                       fluidRow(
+                         box(width = 12,
+                             column(width = 4,
+                                    selectizeInput('bidType', 'Type', 
+                                                   choices = na.omit(unique(allData$Type))
+                                    )),
+                             column(width = 4,
+                                    selectizeInput('bidClarification', 'Type Clarification', 
+                                                   choices = c("") #observed na.omit(unique(allData$Type_Clarification))
+                                    )),
+                             column(
+                               width = 4,
+                               selectizeInput('bidEvaluation', 'Evaluation', 
+                                              choices = c("") #observed #na.omit(unique(allData$EvaluatedBy))
+                               )),
+                             column(
+                               width = 4,
+                               selectizeInput('bidAuction', 'Auction ID', 
+                                              choices = c("") #observed #na.omit(unique(offersInTime$Auction_ID))
+                               )),
+                             column(
+                               width = 4,
+                               selectizeInput('bidItem', 'Item ID', 
+                                              choices = c("") #observed
+                               )),
+                             column(
+                               width = 4,
+                               radioButtons("group", "Docasny button:",
+                                            c("Agregated" = "agregated",
+                                              "Participants" = "participants"))
+                             ),
+                             column(
+                               width = 12,
+                               plotOutput("bidAuctions")),
+                             DT::dataTableOutput('bidDesc')
+                         ))
 )
+
 
 
 # Auctions server.R
@@ -99,8 +95,8 @@ bidRenderAuctionPlot <- function(input,output,session){
     if (nrow(subData) != 0) { 
     if (input$group == "agregated") {
     ggplot(data = subData, aes(x = subData$Change_order, y = subData$New_BID, colour = "red")) +
-      geom_line() +
-      xlab('Change_order') + theme_bw() +   geom_point()
+      geom_line() + guides(color = FALSE, size = FALSE) +
+      xlab('Change_order') + theme_bw() + geom_point()
     }else{
       ggplot(data = subData, aes(x = subData$Change_order, y = subData$New_BID, colour = subData$Participant_ID,group = subData$Participant_ID)) +
         geom_line() +
@@ -108,4 +104,6 @@ bidRenderAuctionPlot <- function(input,output,session){
     }
     }
   }))
+  
+  
 }
