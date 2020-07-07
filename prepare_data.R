a4d7dad317d10c57cd640ef7bc7a2bd17b7d265b
Edita Kišková <edita.kiskova@student.tuke.sk>
2020-07-07 16:00:53 +0200

    Replace prepare_data.R - map

--- prepare_data.R
+++ prepare_data.R
@@ -1,15 +1,9 @@
-# Load data
-#allData = read.csv('./data/HI_ALL.csv', stringsAsFactors = F, sep = ";",check.names = F)
-#offersInTime = read.csv('./data/Offersintim.csv', stringsAsFactors = F, sep = ";",check.names = F)
-#items=read.csv('./data/Item.csv', stringsAsFactors = F, sep = ";",check.names = F)
-
-# UPDATE: uz netreba premenovavat
-
 source("prep_functions.R")
 
 allData = read.csv('./data/HI_ALL.csv', stringsAsFactors = F, sep = ";",check.names = F)
 offersInTime = read.csv('./data/Offersintime.csv', stringsAsFactors = F, sep = ";",check.names = F)
 items=read.csv('./data/Items.csv', stringsAsFactors = F, sep = ";",check.names = F)
+map_data = NULL 
 
 prepareAllData <- function(allData) {
   
@@ -52,58 +46,60 @@ prepareAllData <- function(allData) {
   allData$ID_State[allData$ID_State == 0  ] <- "Other"
   
   #add longitude and latitude
-  
-  unique(allData$ID_State)
-  allData$lng <- ifelse(allData$ID_State == "Slovakia", 19.69902 ,
-                        ifelse(allData$ID_State == "France", 2.213749,
-                               ifelse(allData$ID_State == "Czech Republic", 15.47296,
-                                      ifelse(allData$ID_State == "Finland", 25.74815,
-                                             ifelse(allData$ID_State == "Germany", 10.45153,
-                                                    ifelse(allData$ID_State == "Italy", 12.56738,
-                                                           ifelse(allData$ID_State == "Netherlands", 5.291266,
-                                                                  ifelse(allData$ID_State == "Hungary", 19.5033,
-                                                                         ifelse(allData$ID_State == "Spain", -3.74922,
-                                                                                ifelse(allData$ID_State =="Sweden",18.6435,0))))))))))
-  
-  
-  allData$lat <- ifelse(allData$ID_State == "Slovakia", 48.69903 ,
-                        ifelse(allData$ID_State == "France", 46.22764,
-                               ifelse(allData$ID_State == "Czech Republic", 49.81749,
-                                      ifelse(allData$ID_State == "Finland", 61.92411,
-                                             ifelse(allData$ID_State == "Germany", 51.16569,
-                                                    ifelse(allData$ID_State == "Italy", 41.87194,
-                                                           ifelse(allData$ID_State == "Netherlands", 52.13263,
-                                                                  ifelse(allData$ID_State == "Hungary", 47.16249,
-                                                                         ifelse(allData$ID_State == "Spain", 40.46367, 
-                                                                                ifelse(allData$ID_State == "Sweden", 60.12816,0.1))))))))))
+  return(allData)
+}
+
+prepareMapData <- function(map_data) {
+ 
+  map_data = unique(allData[,c("ID_State")])
+  map_data$lng <- ifelse(map_data$ID_State == "Slovakia", 19.69902 ,
+                         ifelse(map_data$ID_State == "France", 2.213749,
+                                ifelse(map_data$ID_State == "Czech Republic", 15.47296,
+                                       ifelse(map_data$ID_State == "Finland", 25.74815,
+                                              ifelse(map_data$ID_State == "Germany", 10.45153,
+                                                     ifelse(map_data$ID_State == "Italy", 12.56738,
+                                                            ifelse(map_data$ID_State == "Netherlands", 5.291266,
+                                                                   ifelse(map_data$ID_State == "Hungary", 19.5033,
+                                                                          ifelse(map_data$ID_State == "Spain", -3.74922,
+                                                                                 ifelse(map_data$ID_State =="Sweden",18.6435,0))))))))))
   
   
-  allData$max_BID_Value <- ifelse(allData$ID_State == "Slovakia", 562068000 ,
-                                  ifelse(allData$ID_State == "France", 66660000,
-                                         ifelse(allData$ID_State == "Czech Republic", 2500000000,
-                                                ifelse(allData$ID_State == "Finland", 15525529,
-                                                       ifelse(allData$ID_State == "Germany", 11597801,
-                                                              ifelse(allData$ID_State == "Italy", 121094945268,
-                                                                     ifelse(allData$ID_State == "Netherlands", 0,
-                                                                            ifelse(allData$ID_State == "Hungary", 163370,
-                                                                                   ifelse(allData$ID_State == "Spain", 51972150,
-                                                                                          ifelse(allData$ID_State == "Sweden",1425829,3911780))))))))))
+  map_data$lat <- ifelse(map_data$ID_State == "Slovakia", 48.69903 ,
+                         ifelse(map_data$ID_State == "France", 46.22764,
+                                ifelse(map_data$ID_State == "Czech Republic", 49.81749,
+                                       ifelse(map_data$ID_State == "Finland", 61.92411,
+                                              ifelse(map_data$ID_State == "Germany", 51.16569,
+                                                     ifelse(map_data$ID_State == "Italy", 41.87194,
+                                                            ifelse(map_data$ID_State == "Netherlands", 52.13263,
+                                                                   ifelse(map_data$ID_State == "Hungary", 47.16249,
+                                                                          ifelse(map_data$ID_State == "Spain", 40.46367, 
+                                                                                 ifelse(map_data$ID_State == "Sweden", 60.12816,0.1))))))))))
   
   
-  allData$min_BID_Value <- ifelse(allData$ID_State == "Slovakia", -60010 ,
-                                  ifelse(allData$ID_State == "France", 0,
-                                         ifelse(allData$ID_State == "Czech Republic", -2775270,
-                                                ifelse(allData$ID_State == "Finland", 39619.62,
-                                                       ifelse(allData$ID_State == "Germany", 0,
-                                                              ifelse(allData$ID_State == "Italy", -155000,
-                                                                     ifelse(allData$ID_State == "Netherlands", 0,
-                                                                            ifelse(allData$ID_State == "Hungary", 43050,
-                                                                                   ifelse(allData$ID_State == "Spain", 0, 
-                                                                                          ifelse(allData$ID_State == "Sweden",1425829,0.0))))))))))
+  map_data$max_BID_Value <- ifelse(map_data$ID_State == "Slovakia", 562068000 ,
+                                   ifelse(map_data$ID_State == "France", 66660000,
+                                          ifelse(map_data$ID_State == "Czech Republic", 2500000000,
+                                                 ifelse(map_data$ID_State == "Finland", 15525529,
+                                                        ifelse(map_data$ID_State == "Germany", 11597801,
+                                                               ifelse(map_data$ID_State == "Italy", 121094945268,
+                                                                      ifelse(map_data$ID_State == "Netherlands", 0,
+                                                                             ifelse(map_data$ID_State == "Hungary", 163370,
+                                                                                    ifelse(map_data$ID_State == "Spain", 51972150,
+                                                                                           ifelse(map_data$ID_State == "Sweden",1425829,3911780))))))))))
   
+  
+  map_data$min_BID_Value <- ifelse(map_data$ID_State == "Slovakia", -60010 ,
+                                   ifelse(map_data$ID_State == "France", 0,
+                                          ifelse(map_data$ID_State == "Czech Republic", -2775270,
+                                                 ifelse(map_data$ID_State == "Finland", 39619.62,
+                                                        ifelse(map_data$ID_State == "Germany", 0,
+                                                               ifelse(map_data$ID_State == "Italy", -155000,
+                                                                      ifelse(map_data$ID_State == "Netherlands", 0,
+                                                                             ifelse(map_data$ID_State == "Hungary", 43050,
+                                                                                    ifelse(map_data$ID_State == "Spain", 0, 
+                                                                                           ifelse(map_data$ID_State == "Sweden",1425829,0.0))))))))))
   
   #--------------------- end -> MAP
-  return(allData)
 }
 
 prepareOffersInTime <- function(offersInTime) {
